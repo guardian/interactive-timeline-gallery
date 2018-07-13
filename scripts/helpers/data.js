1d138f6a367fdcd632c86e3c154b93d8ccae1cbe
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-07-13 16:20:33 -0400

    convert grid link to url

--- scripts/helpers/data.js
+++ scripts/helpers/data.js
@@ -44,10 +44,7 @@ function cleanMedia(data) {
                 url = url.replace('images/', '');
                 url = url.split('?')[0];
 
-
-
             data.slides[i].media = url + '/' + crop + '/1000.jpg';
-            console.log(data.slides[i].media);
         }
     }
 
