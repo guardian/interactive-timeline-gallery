b9de374e8ca093e9878b0c3a3e28b0ef7f95edec
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-07-13 15:01:23 -0400

    add images

--- scripts/helpers/data.js
+++ scripts/helpers/data.js
@@ -34,12 +34,33 @@ function setSheetNames(data) {
     return data;
 }
 
+function cleanMedia(data) {
+    for (var i in data.slides) {
+        var url = data.slides[i].media;
+        if (url) {
+            var crop = url.split('?crop=')[1];
+                url = url.replace('gutools.co.uk', 'guim.co.uk');
+                url = url.replace('http://', 'https://');
+                url = url.replace('images/', '');
+                url = url.split('?')[0];
+
+
+
+            data.slides[i].media = url + '/' + crop + '/1000.jpg';
+            console.log(data.slides[i].media);
+        }
+    }
+
+    return data;
+}
+
 module.exports = function getData() {
     var isDone = false;
 
     fetchData(function(result) {
         data = result;
         data = setSheetNames(data);
+        data = cleanMedia(data);
 
         isDone = true;
     });
